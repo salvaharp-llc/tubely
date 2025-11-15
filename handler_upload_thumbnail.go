120f392cd0099f81ec320f27371efcc61c8869b5
salvaharp-llc <salvaharp@gmail.com>
2025-11-15 19:25:45 +0100

    new file name to avoid cache

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -59,7 +59,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	assetPath := getAssetPath(videoID, mediaType)
+	assetPath := getAssetPath(mediaType)
 	filePath := cfg.getAssetDiskPath(assetPath)
 
 	dst, err := os.Create(filePath)
